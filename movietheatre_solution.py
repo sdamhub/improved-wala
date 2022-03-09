59d2c24ea39cf5d36a652ce130b0f9162bd075e9
Damilola Solomon <51260627+sdamhub@users.noreply.github.com>
2022-03-09 20:32:50 +0100

    The question
     Movie Theatre
     Problem
    
     A concession stand at a movie theatre sells two products: popcorn for $7 and soda for $2.50.
     If someone buys both on the same day, they get a bundle price of $9 for the two items.
    A customer can purchase any number of bundles on the same day. Customers can run a tab, and
    when settling the bill, we need to calculate how much they owe using the list of past purchases.
    
    Provide a function that, given the list of past purchases as input (string “<date>,<item>“)
    returns the amount owed.
    
     Here is a list of input/output examples:
    
     > tally( [ "2022-06-01,soda",
       ] )
     => 2.5
    
     > tally( [
         "2021-11-15,popcorn",
        "2021-11-15,popcorn",
       "2021-11-15,soda",
        "2021-11-15,soda",
       ] )
     => 18
    
    (7 + 7 + 2 + 2) = 18
    
    > tally( [
        "2019-12-29,popcorn",
        "2020-01-03,popcorn",
       "2020-01-03,soda",
       "2020-01-03,soda",
         "2020-02-22,popcorn",
         "2020-02-22,soda",
       ] ) => 27.5
    
     (7) + (7 + 2 + 2.5) + (7 + 2) = 27.5
    
    > tally( [
        "2019-12-29,soda",
        "2020-01-03,popcorn",
         "2020-01-03,soda",
         "2020-01-03,soda",
       "2020-02-22,popcorn",
        "2020-02-22,soda",
      ] ) => 23
    
     (2.5) + (7 + 2 + 2.5) + (7 + 2) = 23

--- movietheatre_solution.py
+++ movietheatre_solution.py
@@ -1,54 +1,3 @@
-# Movie Theatre
-# Problem
-
-# A concession stand at a movie theatre sells two products: popcorn for $7 and soda for $2.50.
-# If someone buys both on the same day, they get a bundle price of $9 for the two items.
-# A customer can purchase any number of bundles on the same day. Customers can run a tab, and
-# when settling the bill, we need to calculate how much they owe using the list of past purchases.
-
-# Provide a function that, given the list of past purchases as input (string “<date>,<item>“)
-# returns the amount owed.
-
-# Here is a list of input/output examples:
-
-# > tally( [ "2022-06-01,soda",
-#   ] )
-# => 2.5
-
-# > tally( [
-#     "2021-11-15,popcorn",
-#     "2021-11-15,popcorn",
-#     "2021-11-15,soda",
-#     "2021-11-15,soda",
-#   ] )
-# => 18
-
-# (7 + 7 + 2 + 2) = 18
-
-# > tally( [
-#     "2019-12-29,popcorn",
-#     "2020-01-03,popcorn",
-#     "2020-01-03,soda",
-#     "2020-01-03,soda",
-#     "2020-02-22,popcorn",
-#     "2020-02-22,soda",
-#   ] ) => 27.5
-
-# (7) + (7 + 2 + 2.5) + (7 + 2) = 27.5
-
-# > tally( [
-#     "2019-12-29,soda",
-#     "2020-01-03,popcorn",
-#     "2020-01-03,soda",
-#     "2020-01-03,soda",
-#     "2020-02-22,popcorn",
-#     "2020-02-22,soda",
-#  ] ) => 23
-
-# (2.5) + (7 + 2 + 2.5) + (7 + 2) = 23
-
-
-
 def movie_cost(purchase_entry):
     purchase_entry.sort()
     purchase_enter = purchase_entry
@@ -87,4 +36,8 @@ purchase_entry = ["2021-11-15,popcorn",
                   "2021-01-31,popcorn"]
 
 movie_cost(purchase_entry)
+
+
+
+
 
