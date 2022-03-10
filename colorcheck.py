08554a82869b23f4a36009829730fcc665c60b5a
Kolawole Akinwande <kolawole_akinwande@discovery.com>
2022-03-10 12:50:54 +0100

    issue #2 -validation fix and case problem

--- colorcheck.py
+++ colorcheck.py
@@ -2,22 +2,22 @@ VALID_COLORS = ['blue', 'yellow', 'red']
 
 
 def print_colors():
-    """In the while loop ask the user to enter a color,
-       lowercase it and store it in a variable. Next check: 
-       - if 'quit' was entered for color, print 'bye' and break. 
-       - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
-       - otherwise print the color in lower case."""
+    """
+    In the while loop ask the user to enter a color,
+    lowercase it and store it in a variable. Next check: 
+    - if 'quit' was entered for color, print 'bye' and break. 
+    - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
+    - otherwise print the color in lower case.
+    """
     while True:
-        intake = input('enter a color' )
-        intake.lower()
+        intake = input('Enter a color: ').lower()
         if intake == "quit":
             print("bye")
             break
-        if intake not in VALID_COLORS:
+        elif intake not in VALID_COLORS:
             print("Not a valid color")
         else:
-            print(intake.lower())
-        
+            print(intake)
 
-        pass
-print_colors()
+if __name__ == "__main__":  
+    print_colors()
